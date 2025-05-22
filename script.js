d9836fa71c6ec2ae54b9b8fc558289282afbe988
ENZO CASSOL DE DÉA <enzo_dea@estudante.sesisenai.org.br>
2025-05-22 11:04:33 -0300

    codigo comentado e passo 4 adicionado parcialmente

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-const fs = require('fs');
-const { v4: uuidv4 } = require('uuid');
-const express = require('express')
+//imports
+import { appendFile } from 'fs';
+import { v4 as uuidv4 } from 'uuid';
+import express, { json } from 'express';
+//porta do servidor
+const PORTA = 8080
 
 const server = express()
-server.use(express.json())
+server.use(json())
 
 server.options('/', (req, res)=>{
     res.status(200).json({msg:"Tudo Ok"})
 })
-
+// função de caminho e conteudo para o usar no log
 function anexarConteudo(caminho, conteudo) {
   return new Promise((resolve, reject) => {
-    fs.appendFile(caminho, conteudo, 'utf8', (err) => {
+    appendFile(caminho, conteudo, 'utf8', (err) => {
       if (err) {
         reject(err);
       } else {
@@ -20,14 +23,14 @@ function anexarConteudo(caminho, conteudo) {
     });
   });
 }
-
+// adicionar o caminho do log e o conteudo para aparecer no log
 async function anexarAoArquivo() {
   const caminhoDoArquivo = 'logs.txt';
   const IdAleatorio = uuidv4().toString();
   const data = new Date();
   const aluno = "Enzo";
   const resposta = `\nId: ${IdAleatorio}, Data: ${data}, Nome: ${aluno} `
-
+// adicionar o conteudo no log
   try {
     await anexarConteudo(caminhoDoArquivo, resposta);
     console.log('Conteúdo anexado com sucesso!');
@@ -36,9 +39,18 @@ async function anexarAoArquivo() {
   }
   console.log(resposta)
 }
-
+// Funcionando parcialmente 
 anexarAoArquivo();
+server.post('/logs', (req, res) => {
+  const {nome_aluno} = req.body
+      if(!nome_aluno){
+          return res.status(400).json({erro: 'Você não colocou um nome!'})
+      }
+      const idGerado = adicionarMensagem(nome_aluno)
+          res.status(200).json({mensagem: 'Log salvo', id: idGerado})
+})
 
-server.listen(8000, () => {
-    console.log('Servidor rodando na porta 8000')
+//abrir a porta
+server.listen(PORTA, () => {
+    console.log('Servidor rodando na porta 8080')
 })
