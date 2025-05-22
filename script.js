42f4637dd7d62a1cdd82c8768e55ecaeb3ee3b0f
ENZO CASSOL DE DÉA <enzo_dea@estudante.sesisenai.org.br>
2025-05-22 10:02:38 -0300

    Servidor aberto, Criação da API REST com Express e Uuid

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 
+const server = express()
+server.use(express.json())
+
+server.options('/', (req, res)=>{
+    res.status(200).json({msg:"Tudo Ok"})
+})
+server.get('/tarefas', (req, res)=> {
+    fs.readFile('./banco.json', 'utf-8', (err, data) =>{
+        if(err){
+            res.status(500).json({erro:err})
+        } else {
+            const tarefas = JSON.parse(data)
+            res.status(200).json(tarefas)
+        }
+    })
+})
+
 function anexarConteudo(caminho, conteudo) {
   return new Promise((resolve, reject) => {
     fs.appendFile(caminho, conteudo, 'utf8', (err) => {
@@ -30,3 +47,7 @@ async function anexarAoArquivo() {
 }
 
 anexarAoArquivo();
+
+server.listen(8000, () => {
+    console.log('Servidor rodando na porta 8000')
+})
